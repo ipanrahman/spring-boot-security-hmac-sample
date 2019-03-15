f34f5aa7d98cd9c5d04953f8069b11548594cc97
Ipan Taupik Rahman <ipantaupik97@gmail.com>
2019-03-15 13:30:11 +0700

    first commit

---
+++ src/main/java/com/ipan97/springbootauditsample/SpringBootAuditSampleApplication.java
@@ -0,0 +1,13 @@
+package com.ipan97.springbootauditsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAuditSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAuditSampleApplication.class, args);
+	}
+
+}
